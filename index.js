f1dd01c1dd373572e7ee7651dba3dad24047e398
Tobias <tobias.riemenschneider@gmx.net>
2022-09-11 21:19:25 +0200

    Use CLIENT_URL for cors

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ if (process.env.NODE_ENV !== "production") {
     console.log("production code");
 }
 
-const corsOptions = {
-    origin: "*", // client that we allow
-    methods: ["GET", "POST"]
-}
-
 const SERVER_PORT = process.env.PORT || 3001;
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+const corsOptions = {
+    origin: CLIENT_URL,
+    methods: ["GET", "POST"]
+}
+console.log("cors origin:", corsOptions.origin)
 app.use(cors(corsOptions))
 const server = http.createServer(app)
 
