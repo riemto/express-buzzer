1ad60aa89ff3b3b72812e7a82478337979071e44
Tobias <tobias.riemenschneider@gmx.net>
2022-10-20 17:37:14 +0200

    use gameSocket from function scope

--- buzzergame.js
+++ buzzergame.js
@@ -2,7 +2,6 @@ const { ServerStore } = require('./datastore/serverstore')
 let buzzes;
 let unlockedGames;
 let io;
-var socket;
 let serverStore;
 
 exports.initGame = () => {
@@ -19,257 +18,258 @@ exports.initGame = () => {
  * @param gameSocket The socket object for the connected client.
  */
 exports.connectSocket = (sio, gameSocket) => {
-    console.log("connect socket", gameSocket.id)
+    console.log("connect gameSocket", gameSocket.id)
     // Set global variables based on input so that rest of the file can access them.
     io = sio;
-    socket = gameSocket;
 
     // HOST
-    socket.on("hostConnect", hostConnect);
-    socket.on("hostStartGame", hostStartGame);
-    socket.on("hostUnlock", hostUnlock);
-    socket.on("hostPlayerScore", hostPlayerScore);
-    socket.on("hostReset", hostReset);
+    gameSocket.on("hostConnect", hostConnect);
+    gameSocket.on("hostStartGame", hostStartGame);
+    gameSocket.on("hostUnlock", hostUnlock);
+    gameSocket.on("hostPlayerScore", hostPlayerScore);
+    gameSocket.on("hostReset", hostReset);
 
     // PLAYER
-    socket.on("playerConnect", playerConnect);
-    socket.on("playerHitBuzzer", playerHitBuzzer)
-    socket.on("playerSendData", playerSendData);
+    gameSocket.on("playerConnect", playerConnect);
+    gameSocket.on("playerHitBuzzer", playerHitBuzzer)
+    gameSocket.on("playerSendData", playerSendData);
 
     // DEBUG
-    socket.on("debugPing", debugPing);
+    gameSocket.on("debugPing", debugPing);
 
     // GENERAL SOCKET CONNECTION
-    socket.on("disconnecting", disconnecting);
-    socket.on("disconnect", disconnect);
-}
+    gameSocket.on("disconnecting", disconnecting);
+    gameSocket.on("disconnect", disconnect);
 
-/* *******************************
-   *                             *
-   *       HOST FUNCTIONS        *
-   *                             *
-   ******************************* */
 
-/**
- * Create game was clicked and 'hostConnect' event occured.
- * 
- * @param gameId id of game from URL
- * @param name hostname
- */
-async function hostConnect({ gameId, userId }, setPlayers) {
-    console.log(`HOST: ${socket.id}  VS ${userId} joins show page for: ${gameId}`)
-    socket.join(gameId);
-    serverStore.addHost(userId);
-    const players = serverStore.getPlayers(gameId);
-    setPlayers(players.toArray())
-    console.log("PLAYERS WHEN HOST CONNECTS", players.toArray())
-}
+    /* *******************************
+       *                             *
+       *       HOST FUNCTIONS        *
+       *                             *
+       ******************************* */
 
-/**
- * Host clicked the START button and 'hostStartGame' event occured.
- */
-function hostStartGame({ gameId }) {
-    unlockedGames.add(gameId);
-    console.log("started", gameId)
-    console.log("unlocked games:", unlockedGames)
-    io.to(gameId).emit("unlock");
-}
-
-/**
- * Host unlocks all buzzers so that players are ready to hit buzzer
- */
-function hostUnlock({ gameId }) {
-    // reset the buzzers
-    buzzes.delete(gameId);
-    io.to(gameId).emit("unlock")
-}
-
-function hostPlayerScore({ gameId, userId, name, delta }) {
-    const players = serverStore.getPlayers(gameId);
-    const player = players.get(userId);
-    if (!player) {
-        console.error("HOSTPLAYERSCORE", "Player not found with socket id", userId)
-        return;
-    }
-    if (player.name == name) {
-        player.score = (player.score || 0) + delta;
-    }
-    // note: Since players is referenced to player,
-    // players is already up to date.
-    // The serverStore is up to date as well because players is
-    // a reference. So no need to inject the updated player
-    // back into players and players back into serverStore.
-
-    io.to(gameId).emit("playerUpdated", {
-        players: players.toArray()
-    })
-}
-
-function hostReset({ gameId }) {
-    // reset buzzes
-    buzzes.delete(gameId);
-    unlockedGames.delete(gameId);
-
-    // reset players and update host
-    serverStore.reset(gameId);
-    const players = serverStore.getPlayers(gameId);
-
-    io.to(gameId).emit("playerUpdated", {
-        players: players.toArray()
-    })
-}
-
-/* *****************************
-   *                           *
-   *     PLAYER FUNCTIONS      *
-   *                           *
-   ***************************** */
-/**
- * Player connects to the game.
- * @param setGameStatus callback to provide game status to client.
- */
-function playerConnect({ gameId, player }, setGameStatus) {
-    const { name, userId, color } = player;
-    if (!userId) {
-        console.log("PLAYERCONNECT: userId not defined")
-        return;
-    }
-    console.log(name, userId, color)
-    console.log(`PLAYERCONNECT: ${name} - ${userId}`)
-    socket.join(gameId);
-    if (!name || name == "") {
-        console.log("PLAYERCONNECT: but player does not have a name yet")
-        return;
+    /**
+     * Create game was clicked and 'hostConnect' event occured.
+     * 
+     * @param gameId id of game from URL
+     * @param name hostname
+     */
+    async function hostConnect({ gameId, userId }, setPlayers) {
+        console.log(`HOST: ${gameSocket.id}  VS ${userId} joins show page for: ${gameId}`)
+        gameSocket.join(gameId);
+        serverStore.addHost(userId);
+        const players = serverStore.getPlayers(gameId);
+        setPlayers(players.toArray())
+        console.log("PLAYERS WHEN HOST CONNECTS", players.toArray())
     }
 
-    let players = serverStore.getPlayers(gameId);
-    players.updatePlayer(userId, socket.id, player);
-    // const oldVersionOfPlayer = players.get(userId);
-    // const updatedPlayer = { ...oldVersionOfPlayer, ...player };
-    // players.set(userId, updatedPlayer);
-
-    // TODO: is this neeeded:
-    serverStore.setPlayers(gameId, players);
-
-
-    console.log("PLAYERCONNECT: players in game", players.toArray())
-    // if buzzer already pressed at moment where socket joins
-    // notify him so he sees same results as others.
-    if (buzzes.has(gameId)) {
-        const firstBuzzData = buzzes.get(gameId);
-        io.to(socket.id).emit("showBuzzerData", { ...firstBuzzData, buzzerDataComplete: true })
-        // emit a note as well so he is aware of what he is seeing
-        io.to(socket.id).emit("notifyLatecomer")
+    /**
+     * Host clicked the START button and 'hostStartGame' event occured.
+     */
+    function hostStartGame({ gameId }) {
+        unlockedGames.add(gameId);
+        console.log("started", gameId)
+        console.log("unlocked games:", unlockedGames)
+        io.to(gameId).emit("unlock");
     }
 
-    if (unlockedGames.has(gameId)) {
-        io.to(socket.id).emit("unlock")
+    /**
+     * Host unlocks all buzzers so that players are ready to hit buzzer
+     */
+    function hostUnlock({ gameId }) {
+        // reset the buzzers
+        buzzes.delete(gameId);
+        io.to(gameId).emit("unlock")
     }
-    if (setGameStatus) {
-        setGameStatus({ unlocked: unlockedGames.has(gameId) })
-    }
 
-    // Inform rest that the player connected
-    io.to(gameId).emit("playerUpdated", {
-        players: players.toArray()
-    })
-}
+    function hostPlayerScore({ gameId, userId, name, delta }) {
+        const players = serverStore.getPlayers(gameId);
+        const player = players.get(userId);
+        if (!player) {
+            console.error("HOSTPLAYERSCORE", "Player not found with user id", userId)
+            return;
+        }
+        if (player.name == name) {
+            player.score = (player.score || 0) + delta;
+        }
+        // note: Since players is referenced to player,
+        // players is already up to date.
+        // The serverStore is up to date as well because players is
+        // a reference. So no need to inject the updated player
+        // back into players and players back into serverStore.
 
-function playerHitBuzzer({ gameId, name, color, userId },
-    correctServerTimeAndEmitDataToServer) {
-    const timeBuzzReceivedOnServer = Date.now();
-    // send server time back to client so it can compute the latency
-    // and inform us back about that with a new emit.
-    correctServerTimeAndEmitDataToServer(timeBuzzReceivedOnServer);
-
-    if (!buzzes.has(gameId)) {
-        // broadcast to others as well. Own socket is already
-        // displaying info to have faster response experience.
-        socket.to(gameId).emit("showBuzzerData", {
-            name,
-            color,
-            buzzerDataComplete: false,
-            userId
+        io.to(gameId).emit("playerUpdated", {
+            players: players.toArray()
         })
     }
-}
 
-function playerSendData({ gameId, name, color, timestamp, userId }) {
-    if (buzzes.has(gameId)) {
-        const firstBuzzData = buzzes.get(gameId);
-        if (name !== firstBuzzData.name) {
-            const delta = timestamp - firstBuzzData.timestamp;
-            if (delta > 0) {
-                console.log("too late. Buzzes", buzzes)
-                io.to(socket.id).emit("notifyPlayerIsTooLate", { firstBuzzData, delta })
-            } else {
-                // the connection was just too late. Inform everybody.
-                buzzes.set(gameId, { name, color, timestamp, userId, buzzerDataComplete: true })
-                const deltaSeconds = delta / 1000;
-                const deltaSecondsRounded = Math.round(deltaSeconds * 10) / 10;
-                const alertMessage = `
+    function hostReset({ gameId }) {
+        // reset buzzes
+        buzzes.delete(gameId);
+        unlockedGames.delete(gameId);
+
+        // reset players and update host
+        serverStore.reset(gameId);
+        const players = serverStore.getPlayers(gameId);
+
+        io.to(gameId).emit("playerUpdated", {
+            players: players.toArray()
+        })
+    }
+
+    /* *****************************
+       *                           *
+       *     PLAYER FUNCTIONS      *
+       *                           *
+       ***************************** */
+    /**
+     * Player connects to the game.
+     * @param setGameStatus callback to provide game status to client.
+     */
+    function playerConnect({ gameId, player }, setGameStatus) {
+        const { name, userId, color } = player;
+        if (!userId) {
+            console.log("PLAYERCONNECT: userId not defined")
+            return;
+        }
+        console.log(name, userId, color)
+        console.log(`PLAYERCONNECT: ${name} - ${userId}`)
+        gameSocket.join(gameId);
+        if (!name || name == "") {
+            console.log("PLAYERCONNECT: but player does not have a name yet")
+            return;
+        }
+
+        let players = serverStore.getPlayers(gameId);
+        players.updatePlayer(userId, gameSocket.id, player);
+        // const oldVersionOfPlayer = players.get(userId);
+        // const updatedPlayer = { ...oldVersionOfPlayer, ...player };
+        // players.set(userId, updatedPlayer);
+
+        // TODO: is this neeeded:
+        serverStore.setPlayers(gameId, players);
+
+
+        console.log("PLAYERCONNECT: players in game", players.toArray())
+        // if buzzer already pressed at moment where gameSocket joins
+        // notify him so he sees same results as others.
+        if (buzzes.has(gameId)) {
+            const firstBuzzData = buzzes.get(gameId);
+            io.to(gameSocket.id).emit("showBuzzerData", { ...firstBuzzData, buzzerDataComplete: true })
+            // emit a note as well so he is aware of what he is seeing
+            io.to(gameSocket.id).emit("notifyLatecomer")
+        }
+
+        if (unlockedGames.has(gameId)) {
+            io.to(gameSocket.id).emit("unlock")
+        }
+        if (setGameStatus) {
+            setGameStatus({ unlocked: unlockedGames.has(gameId) })
+        }
+
+        // Inform rest that the player connected
+        io.to(gameId).emit("playerUpdated", {
+            players: players.toArray()
+        })
+    }
+
+    function playerHitBuzzer({ gameId, name, color, userId },
+        correctServerTimeAndEmitDataToServer) {
+        const timeBuzzReceivedOnServer = Date.now();
+        // send server time back to client so it can compute the latency
+        // and inform us back about that with a new emit.
+        correctServerTimeAndEmitDataToServer(timeBuzzReceivedOnServer);
+
+        if (!buzzes.has(gameId)) {
+            // broadcast to others as well. Own socket is already
+            // displaying info to have faster response experience.
+            gameSocket.to(gameId).emit("showBuzzerData", {
+                name,
+                color,
+                buzzerDataComplete: false,
+                userId
+            })
+        }
+    }
+
+    function playerSendData({ gameId, name, color, timestamp, userId }) {
+        if (buzzes.has(gameId)) {
+            const firstBuzzData = buzzes.get(gameId);
+            if (name !== firstBuzzData.name) {
+                const delta = timestamp - firstBuzzData.timestamp;
+                if (delta > 0) {
+                    console.log("too late. Buzzes", buzzes)
+                    io.to(gameSocket.id).emit("notifyPlayerIsTooLate", { firstBuzzData, delta })
+                } else {
+                    // the connection was just too late. Inform everybody.
+                    buzzes.set(gameId, { name, color, timestamp, userId, buzzerDataComplete: true })
+                    const deltaSeconds = delta / 1000;
+                    const deltaSecondsRounded = Math.round(deltaSeconds * 10) / 10;
+                    const alertMessage = `
                 Sorry, actually, ${name} was ${-deltaSecondsRounded}s faster.
                 Apparently the connection was slow. Sorry for the slowroll...`;
-                io.to(gameId).emit("showBuzzerData", {
-                    name,
-                    color,
-                    alertMessage,
-                    buzzerDataComplete: true,
-                    userId
-                })
+                    io.to(gameId).emit("showBuzzerData", {
+                        name,
+                        color,
+                        alertMessage,
+                        buzzerDataComplete: true,
+                        userId
+                    })
+                }
+            } else {
+                // that player was actually the first person to buzz. Just show info again.
+                io.to(gameSocket.id).emit("showBuzzerData", firstBuzzData)
             }
         } else {
-            // that player was actually the first person to buzz. Just show info again.
-            io.to(socket.id).emit("showBuzzerData", firstBuzzData)
+            // first buzzer!
+            buzzes.set(gameId, { name, color, timestamp, userId, buzzerDataComplete: true })
+            io.to(gameId).emit("showBuzzerData", {
+                name,
+                color,
+                buzzerDataComplete: true,
+                userId
+            })
         }
-    } else {
-        // first buzzer!
-        buzzes.set(gameId, { name, color, timestamp, userId, buzzerDataComplete: true })
-        io.to(gameId).emit("showBuzzerData", {
-            name,
-            color,
-            buzzerDataComplete: true,
-            userId
-        })
     }
-}
 
-/* *****************************
-   *                           *
-   *   CONNECTION FUNCTIONS    *
-   *                           *
-   ***************************** */
+    /* *****************************
+       *                           *
+       *   CONNECTION FUNCTIONS    *
+       *                           *
+       ***************************** */
 
-/**
- * This function is called right before disconnecting
- */
-function disconnecting(reason) {
-    console.log("disconnecting", socket.id);
-    console.log("REASON", reason)
-    console.log("SOCKET ROOMS: ", socket.rooms);
-    if (serverStore.isHost(socket.id)) {
-        console.log("------- DISCONNECT HOST! --------")
-        serverStore.removeHost(socket.id)
-    } else {
-        // Remove socket from players and update game
-        serverStore.remove(socket.id, (players, gameId) => {
-            console.log("REMOVE PLAYER -> INFORM EVERYBODY IN GAME", gameId, players)
-            io.to(gameId).emit("playerUpdated", players)
-        })
+    /**
+     * This function is called right before disconnecting
+     */
+    function disconnecting(reason) {
+        console.log("disconnecting", gameSocket.id);
+        console.log("REASON", reason)
+        console.log("SOCKET ROOMS: ", gameSocket.rooms);
+        if (serverStore.isHost(gameSocket.id)) {
+            console.log("------- DISCONNECT HOST! --------")
+            serverStore.removeHost(gameSocket.id)
+        } else {
+            // Remove gameSocket from players and update game
+            serverStore.remove(gameSocket.id, (players, gameId) => {
+                console.log("REMOVE PLAYER -> INFORM EVERYBODY IN GAME", gameId, players)
+                io.to(gameId).emit("playerUpdated", players)
+            })
+        }
     }
-}
 
-function disconnect() {
-    // automatically leaves all rooms
-    console.log(`User disconnected: ${socket.id}`)
-}
+    function disconnect() {
+        // automatically leaves all rooms
+        console.log(`User disconnected: ${gameSocket.id}`)
+    }
 
-/* *****************************
-   *                           *
-   *   DEBUGGING FUNCTIONS     *
-   *                           *
-   ***************************** */
-function debugPing(callback) {
-    // used for debugging
-    callback();
+    /* *****************************
+       *                           *
+       *   DEBUGGING FUNCTIONS     *
+       *                           *
+       ***************************** */
+    function debugPing(callback) {
+        // used for debugging
+        callback();
+    }
+
 }
